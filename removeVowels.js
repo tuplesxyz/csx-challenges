71fa306bd262e4ff89644004ad865c642194f8d2
tuplesxyz <71386269+tuplesxyz@users.noreply.github.com>
2022-01-04 18:17:39 +0000

    add challenge

---
+++ removeVowels.js
@@ -0,0 +1,9 @@
+function disemvowel(string) {
+  // ADD CODE HERE
+  return string.replace(/[aeiou]/gi, "");
+}
+
+// Uncomment these to check your work!
+// console.log(disemvowel('CodeSmith')); // => 'CdSmth'
+// console.log(disemvowel('BANANA')); // => 'BNN'
+// console.log(disemvowel('hello world')); // => 'hll wrld'
