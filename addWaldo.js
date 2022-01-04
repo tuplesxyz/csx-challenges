c11b75c9a18e3b4c46b3c05267c74cb8d36c0659
tuplesxyz <71386269+tuplesxyz@users.noreply.github.com>
2022-01-04 18:20:50 +0000

    add challenge

---
+++ addWaldo.js
@@ -0,0 +1,8 @@
+// ADD CODE HERE
+function addWaldo(obj) {
+  obj["Waldo"] = "unknown";
+  return obj;
+}
+// Uncomment these to check your work!
+// const siliconValley = {'Richard': 'Hendricks', 'Erlich': 'Bachman', 'Bertram': 'Gilfoyle'}
+// console.log(addWaldo(siliconValley)) // should log:{ Richard: 'Hendricks', Erlich: 'Bachman', Bertram: 'Gilfoyle', Waldo: 'unknown' }
