f03902c181c105aef3c730a65e3468cc3f305a31
tuplesxyz <71386269+tuplesxyz@users.noreply.github.com>
2022-01-04 17:57:33 +0000

    add challenge

---
+++ breakingOutOfALoop.js
@@ -0,0 +1,16 @@
+function holidays(arr) {
+  // Do not use a variable to store your result
+  // ADD CODE HERE
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] == "October") {
+      return "Happy Halloween";
+    }
+  }
+  return "Have a great day!";
+}
+
+// Uncomment these to check your work!
+// const months = ["April", "May", "June", "October"];
+// const animals = ["Cats", "Dogs", "Pigs"];
+// console.log(holidays(months)); // should return: "Happy Halloween"
+// console.log(holidays(animals)); // should return: "Have a great day!"
