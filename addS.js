f0963a1a8b046f62ceaa97eff883bd7a36d7fab4
tuplesxyz <71386269+tuplesxyz@users.noreply.github.com>
2022-01-04 17:57:25 +0000

    add challenge

---
+++ addS.js
@@ -0,0 +1,7 @@
+// ADD CODE HERE
+function addS(str) {
+  return str + "s";
+}
+// Uncomment these to check your work!
+// console.log(typeof addS); // should log: 'function'
+// console.log(addS('cat')); // should log: 'cats'
