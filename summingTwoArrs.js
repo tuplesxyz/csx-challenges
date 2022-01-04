22fbec7c4e52783cd69a9f502abd6f12fea5b4fe
tuplesxyz <71386269+tuplesxyz@users.noreply.github.com>
2022-01-04 17:59:28 +0000

    add challenge

---
+++ summingTwoArrs.js
@@ -0,0 +1,8 @@
+function mergingElements(array1, array2) {
+  // ADD CODE HERE
+  return array1.map((a, i) => a + array[i]);
+}
+
+// Uncomment these to check your work!
+// console.log(mergingElements([1, 2, 3, 4], [5, 6, 7, 8])); // expected log [6, 8, 10, 12]
+// console.log(mergingElements([7, 3, 6, 0], [3, 9, 17, 81])); // expected log [10, 12, 23, 81]
