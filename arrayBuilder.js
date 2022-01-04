6e5beaa6a6b7fface1fd35d5ebf8db98cbff9a0f
tuplesxyz <71386269+tuplesxyz@users.noreply.github.com>
2022-01-04 18:31:52 +0000

    add challenge solution

---
+++ arrayBuilder.js
@@ -0,0 +1,15 @@
+function arrayBuilder(obj) {
+  // ADD CODE HERE
+  let arr = [];
+  for (let k in obj) {
+    while (obj[k] > 0) {
+      arr.push(k);
+      obj[k] -= 1;
+    }
+  }
+  return arr;
+}
+
+// Uncomment these to check your work!
+console.log(arrayBuilder({ cats: 2, dogs: 1 })); // => ['cats', 'cats', 'dogs']
+// console.log(arrayBuilder({})); // => []
