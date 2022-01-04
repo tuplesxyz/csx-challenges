5ad0299a33f707254e420725245ec8a87b53f22d
tuplesxyz <71386269+tuplesxyz@users.noreply.github.com>
2022-01-04 17:59:34 +0000

    add challenge

---
+++ wereAwesome.js
@@ -0,0 +1,9 @@
+function wereAwesome(you, yourBuddy) {
+  return `${yourBuddy} and ${you} are awesome!`;
+}
+
+// Uncomment the lines below to test your function:
+// console.log(wereAwesome("Dave", "Will")); // => "Will and Dave are awesome!"
+// console.log(wereAwesome("Victoria", "Jenny")); // => "Jenny and Victoria are awesome!"
+// console.log(wereAwesome("Chris", "Jac")); // => "Jac and Chris are awesome!"
+// console.log(wereAwesome("Phillip", "Skyler")); // => "Skyler and Phillip are awesome!"
