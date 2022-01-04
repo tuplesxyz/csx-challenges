f1d6c10b43ae6b283435339c26ebc1729f158ae3
tuplesxyz <71386269+tuplesxyz@users.noreply.github.com>
2022-01-04 18:22:26 +0000

    add challenge

---
+++ findWaldo.js
@@ -0,0 +1,9 @@
+// ADD CODE HERE
+function findWaldo(obj) {
+  return obj["Waldo"] !== undefined ? obj["Waldo"] : "Where's Waldo?";
+}
+// Uncomment these to check your work!
+// const DC = {'Bruce': 'Wayne', 'Harley': 'Quinn'}
+// const supernatural = {'Sam': 'Winchester', 'Dean': 'Winchester', 'Waldo': 'unknown'}
+// console.log(findWaldo(DC)) // should log: 'Where's Waldo?'
+// console.log(findWaldo(supernatural)) // should log: 'unknown'
