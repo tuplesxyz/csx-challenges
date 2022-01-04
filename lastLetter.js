e5eefc9aa6cecd95e91bb10f410a4b63ce1630e1
tuplesxyz <71386269+tuplesxyz@users.noreply.github.com>
2022-01-04 17:59:01 +0000

    add challenge

---
+++ lastLetter.js
@@ -0,0 +1,9 @@
+function lastLetter(word) {
+  return word[word.length - 1];
+}
+
+// Uncomment the lines below to test your code
+// console.log(lastLetter("hello")); //=> "o"
+// console.log(lastLetter("goodbye!")); //=> "!"
+// console.log(lastLetter("ZeltoiD")); //=> "D"
+// console.log(lastLetter("I love Javascript")); //=> "t"
