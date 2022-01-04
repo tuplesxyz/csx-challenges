a0ce7d53b7843dd2dcaa1c1af9f355e38d4b608f
tuplesxyz <71386269+tuplesxyz@users.noreply.github.com>
2022-01-04 17:46:25 +0000

    add solutionto addTwo

---
+++ addTwo.js
@@ -0,0 +1,7 @@
+// ADD CODE HERE
+function addTwo(x) {
+  return x + 2;
+}
+// Uncomment these to check your work!
+// console.log(typeof addTwo); // should log: 'function'
+// console.log(addTwo(10)); // should log: 12
