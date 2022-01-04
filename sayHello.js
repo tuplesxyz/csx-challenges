e79312c05a54bb38a28a85568d06988809c305a8
tuplesxyz <71386269+tuplesxyz@users.noreply.github.com>
2022-01-04 17:59:26 +0000

    add challenge

---
+++ sayHello.js
@@ -0,0 +1,7 @@
+// ADD CODE HERE
+function sayHello(str) {
+  return `Hi, ${str}`;
+}
+// Uncomment these to check your work!
+// console.log(sayHello('Mary')); // should log: 'Hi, Mary'
+// console.log(sayHello('Haley')); // should log: 'Hi, Haley'
