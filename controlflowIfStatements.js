ee35afc0c95efbaa2360a98794b4ff9bfabb9142
tuplesxyz <71386269+tuplesxyz@users.noreply.github.com>
2022-01-04 17:57:50 +0000

    add challenge

---
+++ controlflowIfStatements.js
@@ -0,0 +1,6 @@
+// ADD CODE HERE
+const iLoveEvenNumbers = (num) =>
+  num % 2 === 0 ? "Oh Yeah Evens!" : "I am too normal for odd numbers";
+// Uncomment these to check your work!
+// console.log(iLoveEvenNumbers(42)); // expected log 'Oh Yeah Evens!'
+// console.log(iLoveEvenNumbers(17)); // expected log 'I am too normal for odd numbers'
