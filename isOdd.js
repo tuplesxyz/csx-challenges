d933a5309bcb6e56af204eda422fbefa858cc6d4
tuplesxyz <71386269+tuplesxyz@users.noreply.github.com>
2022-01-04 17:58:53 +0000

    add challenge

---
+++ isOdd.js
@@ -0,0 +1,5 @@
+// ADD CODE HERE
+const isOdd = (num) => num % 2 === 1;
+// Uncomment these to check your work!
+// console.log(isOdd(5)); // should log: true
+// console.log(isOdd(2008)); // should log: false
