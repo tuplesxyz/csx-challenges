17c4d9d5bfd012de1f9f38022999a91f560834ef
tuplesxyz <71386269+tuplesxyz@users.noreply.github.com>
2022-01-07 09:10:02 +0000

    uncomment logs

--- addS.js
+++ addS.js
@@ -3,5 +3,5 @@ function addS(str) {
   return str + "s";
 }
 // Uncomment these to check your work!
-// console.log(typeof addS); // should log: 'function'
-// console.log(addS('cat')); // should log: 'cats'
+console.log(typeof addS); // should log: 'function'
+console.log(addS("cat")); // should log: 'cats'
