5d86eee794daa3b2acdfaf11a9e38b6d527b1256
tuplesxyz <71386269+tuplesxyz@users.noreply.github.com>
2022-01-07 09:10:35 +0000

    uncomment logs

--- addingAllTheWeirdStuff.js
+++ addingAllTheWeirdStuff.js
@@ -30,4 +30,4 @@ function addingAllTheWeirdStuff(array1, array2) {
 
 // Uncomment these to check your work!
 console.log(addingAllTheWeirdStuff([1, 3, 5, 17, 15], [1, 2, 3, 4, 5])); // expected log [10, 12, 14, 23, 21]
-// console.log(addingAllTheWeirdStuff([1, 3, 5, 17, 15, 1], [1, 2, 3, 4, 5, 22])); // expected log [11, 13, 15, 46, 44, 11]
+console.log(addingAllTheWeirdStuff([1, 3, 5, 17, 15, 1], [1, 2, 3, 4, 5, 22])); // expected log [11, 13, 15, 46, 44, 11]
