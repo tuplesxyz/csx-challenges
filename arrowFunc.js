b39635a9563fabc5407e0a552056c2f77e6ddf1f
tuplesxyz <71386269+tuplesxyz@users.noreply.github.com>
2022-01-07 09:11:03 +0000

    uncomment logs

--- arrowFunc.js
+++ arrowFunc.js
@@ -2,4 +2,4 @@ const myJob = (name, profession) => {
   return `Hi, my name is ${name} and I'm a ${profession}.`;
 };
 
-// console.log(myJob("Jon", "Knights Watchman"));
+console.log(myJob("Jon", "Knights Watchman"));
