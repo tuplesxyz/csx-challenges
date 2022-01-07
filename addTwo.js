dbc070696522cddf333c611a01bc15fe6798b53e
tuplesxyz <71386269+tuplesxyz@users.noreply.github.com>
2022-01-07 09:10:20 +0000

    uncomment logs

--- addTwo.js
+++ addTwo.js
@@ -3,5 +3,5 @@ function addTwo(x) {
   return x + 2;
 }
 // Uncomment these to check your work!
-// console.log(typeof addTwo); // should log: 'function'
-// console.log(addTwo(10)); // should log: 12
+console.log(typeof addTwo); // should log: 'function'
+console.log(addTwo(10)); // should log: 12
