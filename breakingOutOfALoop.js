c2bdf7061362900f3666bc775c0cbfa9c1b6c0ca
tuplesxyz <71386269+tuplesxyz@users.noreply.github.com>
2022-01-07 09:11:13 +0000

    uncomment logs

--- breakingOutOfALoop.js
+++ breakingOutOfALoop.js
@@ -10,7 +10,7 @@ function holidays(arr) {
 }
 
 // Uncomment these to check your work!
-// const months = ["April", "May", "June", "October"];
-// const animals = ["Cats", "Dogs", "Pigs"];
-// console.log(holidays(months)); // should return: "Happy Halloween"
-// console.log(holidays(animals)); // should return: "Have a great day!"
+const months = ["April", "May", "June", "October"];
+const animals = ["Cats", "Dogs", "Pigs"];
+console.log(holidays(months)); // should return: "Happy Halloween"
+console.log(holidays(animals)); // should return: "Have a great day!"
