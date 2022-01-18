91e0cced02ffb79f8ec888a5a394a5f5b933c3ef
tuplesxyz <71386269+tuplesxyz@users.noreply.github.com>
2022-01-18 10:31:21 +0000

    uncomment tests

--- findWaldo.js
+++ findWaldo.js
@@ -2,8 +2,12 @@
 function findWaldo(obj) {
   return obj["Waldo"] !== undefined ? obj["Waldo"] : "Where's Waldo?";
 }
-// Uncomment these to check your work!
-// const DC = {'Bruce': 'Wayne', 'Harley': 'Quinn'}
-// const supernatural = {'Sam': 'Winchester', 'Dean': 'Winchester', 'Waldo': 'unknown'}
-// console.log(findWaldo(DC)) // should log: 'Where's Waldo?'
-// console.log(findWaldo(supernatural)) // should log: 'unknown'
+
+const DC = { Bruce: "Wayne", Harley: "Quinn" };
+const supernatural = {
+  Sam: "Winchester",
+  Dean: "Winchester",
+  Waldo: "unknown",
+};
+console.log(findWaldo(DC)); // should log: 'Where's Waldo?'
+console.log(findWaldo(supernatural)); // should log: 'unknown'
